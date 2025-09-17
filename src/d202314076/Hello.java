7b174fd8985022b4291e602d5e1c76211c7c0da6
Kim, Hangyeol <gyeol3474@gmail.com>
2025-09-17 15:09:56 +0900

    four

--- src/d202314076/Hello.java
+++ src/d202314076/Hello.java
@@ -2,6 +2,7 @@ package d202314076;
 
 public class Hello {
 	public static void run() {
-		System.out.println("202314076"); 
+		System.out.println("202314076");
+		System.out.println("hello world");
 	}
 }
